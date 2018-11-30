907e59621f67ef246a844c930e0c65844e228fa8
Francesco Schwarz <francesco.schwarz@posteo.de>
2018-11-30 23:53:23 +0100

    Change permalinks only in production mode

--- config/filters/permalink.ts
+++ config/filters/permalink.ts
@@ -1,8 +1,13 @@
 /**
- * Removes (index).html from a string
+ * Removes (index).html from a string in production mode
  *
  * @param {String} str URL, i.e. /page/index.html
  * @return {String} Permalinkable URL, i.e. /page/
  *
  */
-module.exports = str => str.replace(/(?:index)?\.html/g, '/');
+module.exports = str => {
+  if (process.env.ELEVENTY_ENV === 'production') {
+    return str.replace(/(?:index)?\.html/g, '/');
+  }
+  return str;
+};
